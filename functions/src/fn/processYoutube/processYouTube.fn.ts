78f9226878822222fd884c41230ef2c33891a50d
kangzeroo <kangzeroo@gmail.com>
2021-06-07 18:43:20 +0800

    video & audio saves successfully to GCS with access token

--- functions/src/fn/processYoutube/processYouTube.fn.ts
+++ functions/src/fn/processYoutube/processYouTube.fn.ts
@@ -13,27 +13,46 @@ const processYouTube = functions.https.onRequest(async (req, res) => {
       endTime && {
         range: { start: startTime, end: endTime },
       }),
+    filter: (format: any) => format.container === "mp4",
   };
+  const downloadToken = uuidv4();
   console.log("Attempting to upload youtube video to google cloud storage....");
 
+  // ytdl() --> createWriteStream()
   // https://github.com/fent/node-ytdl-core
   // https://googleapis.dev/nodejs/storage/latest/File.html#createWriteStream
   const destinationPath = `${USER_ID}/raw-youtube/${uuidv4()}.mp4`;
   const file = bucket.file(destinationPath);
+
+  // allow private access to this file using a "download token"
+  // https://weekly.elfitz.com/2020/06/03/make-your-firebase-storage-files-available-on-upload/
+  const publicUrl = `https://firebasestorage.googleapis.com/v0/b/${
+    bucket.name
+  }/o/${encodeURI(destinationPath).replace(
+    /\//g,
+    "%2F"
+  )}?alt=media&token=${downloadToken}`;
+
+  const writeStream = file.createWriteStream({
+    metadata: {
+      metadata: {
+        firebaseStorageDownloadTokens: downloadToken,
+      },
+    },
+  });
   ytdl(url, options)
-    .pipe(file.createWriteStream())
-    .on("error", (err) => {
+    .pipe(writeStream)
+    .on("error", (err: Error) => {
       console.error(err);
       res.status(400).send({
-        message: `Failed to upload youtube video ${url} to Google Cloud Storage at location: ${"filePath"}`,
+        message: `Failed to upload youtube video ${url} to Google Cloud Storage at location: ${publicUrl}`,
         error: err,
       });
     })
-    .on("finish", (data: any) => {
+    .on("finish", () => {
       console.log("Successfully uploaded");
-      console.log(data);
       res.status(200).send({
-        message: `Successfully uploaded youtube video ${url} to Google Cloud Storage at location: ${"filePath"}`,
+        message: `Successfully uploaded youtube video ${url} to Google Cloud Storage at location: ${publicUrl}`,
       });
     });
 });
